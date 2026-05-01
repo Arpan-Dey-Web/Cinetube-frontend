eac9b3877985a5d6db2f7d5e5b537f92605a6ba1
Arpan Dey <arpandey.web@gmail.com>
2026-05-01 15:03:04 +0600

    feat(layout): wrap public layout with Stripe provider

--- src/app/(RootLayout)/layout.tsx
+++ src/app/(RootLayout)/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar";
 import { AuthProvider } from "@/provider/auth-provider";
+import { StripeProvider } from "@/provider/StripeProvider";
 
 export default function HomePageLayout({
   children,
@@ -9,11 +10,13 @@ export default function HomePageLayout({
 }>) {
   return (
     <div>
-      <AuthProvider>
-        <Navbar />
-        {children}
-        <Footer />
-      </AuthProvider>
+      <StripeProvider>
+        <AuthProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </AuthProvider>
+      </StripeProvider>
     </div>
   );
 }
